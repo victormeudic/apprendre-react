6eb8ff5f511f399a8ab976a18636797773f97034
Victor Meudic <victormeudic@gamil.com>
2022-06-01 21:49:54 +0200

    Router & Navigation

---
+++ src/pages/About.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione numquam
+        obcaecati error itaque, magni ex eos repellat iste facilis unde atque
+        hic officiis accusamus quisquam voluptatum odit illo, nihil fuga?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia molestias
+        ratione deleniti nihil similique exercitationem suscipit aperiam unde
+        autem ipsam, officia voluptatibus? Expedita, voluptates earum?
+        Exercitationem delectus ducimus eos tempore.
+      </p>
+    </div>
+  );
+};
+
+export default About;
